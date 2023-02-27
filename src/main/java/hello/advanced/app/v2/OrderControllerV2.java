adfe1a045a4427b5f846a076844ef7a1cc587140
leesanghun <s2lsh90@gmail.com>
2023-02-27 22:23:15 +0900

    v2

--- src/main/java/hello/advanced/app/v2/OrderControllerV2.java
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -11,15 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderControllerV2 {
     private final OrderServiceV2 orderService;
     private final HelloTraceV2 trace;
+
     @GetMapping("/v2/request")
-    public String request(String itemId){
+    public String request(String itemId) {
         TraceStatus status = null;
         try {
-            status=trace.begin("OrderController.request()");
+            status = trace.begin("OrderController.request()");
             orderService.orderItem(status.getTraceId(), itemId);
             trace.end(status);
             return "ok";
-        }catch (Exception e) {
+        } catch (Exception e) {
             trace.exception(status, e);
             throw e;
         }
--- src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OrderServiceV2
-{
+public class OrderServiceV2 {
     private final OrderRepositoryV2 orderRepository;
     private final HelloTraceV2 trace;
-    public void orderItem(TraceId traceId, String itemId){
+
+    public void orderItem(TraceId traceId, String itemId) {
         TraceStatus status = null;
         try {
-            status=trace.beginSync(traceId,"OrderServiceV2.orderItem()");
-            orderRepository.save(traceId,itemId);
+            status = trace.beginSync(traceId, "OrderServiceV2.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
             trace.end(status);
-        }catch (Exception e) {
+        } catch (Exception e) {
             trace.exception(status, e);
             throw e;
         }
